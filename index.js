fbc9d3f7a9826cdb9a0bf0b861c4311d010a24a6
Shashw1t <iamsoranic@gmail.com>
2025-08-29 11:05:34 +0530

    bfhl

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const bfhlRoutes = require('./routes/bfhlRoutes');
 
 const app = express();
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Make a POST request to /bfhl with a JSON body to use this API. By yours truly- Shashwat Dwivedi');
+});
+
 app.use('/', bfhlRoutes);
 
 const PORT = process.env.PORT || 3000;
